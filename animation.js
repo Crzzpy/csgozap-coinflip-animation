2ed4b53e450b71a99e7c203ccf0b080933937b7a
Crzzpy <41648222+Crzzpy@users.noreply.github.com>
2022-04-19 01:51:47 +0200

    uploaded files

---
+++ animation.js
@@ -0,0 +1,28 @@
+setTimeout(spinDaCoin,2000);
+
+function spinDaCoin() {
+	var intervalz = setInterval(function() {
+		
+		//TODO CURRENT ENDPOS = -600px -1200px
+
+		var elemento = $(".flip-animation");
+		var old_x = parseInt(elemento.css("background-position").split(" ")[0].split("px")[0]);
+		var old_y = parseInt(elemento.css("background-position").split(" ")[1].split("px")[0]);
+		var new_x = old_x;
+		var new_y = old_y;
+
+		if(old_x == -600 && old_y == -1200) {
+			clearInterval(intervalz);
+		} else {
+
+			if(old_x == -3600 && old_y !== -1200) {
+				new_x = 0;
+				new_y = old_y - 200;
+			} else if (old_x !== -3600) {
+				new_x = new_x - 200;
+			}
+
+			elemento.css("background-position",new_x+"px "+new_y+"px");
+		}
+	},55)
+}
